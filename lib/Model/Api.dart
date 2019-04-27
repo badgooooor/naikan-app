5d1b4562ca51124a8ae3bf31064d564fb992a1ae
Wichitchai <60010933@kmitl.ac.th>
2019-04-27 21:18:20 +0700

    :alien: Done with API
    API from firebase backend is finished.

--- lib/Model/Api.dart
+++ lib/Model/Api.dart
@@ -82,6 +82,23 @@ class Api{
     // print(allSnapshot);
   return allSnapshot;
   }
+  Future<List<Snapshot>> getSnapshotYearMonth(int year,int month) async{
+    String urlGetYearMonth = 'https://us-central1-naikan-87838.cloudfunctions.net/webApi/api/v1/snapshots/monthSnapshot/$year/$month';
+    client.Response response = await client.get(urlGetYearMonth,headers: {"Content-Type" : "application/json"});
+    print(json.decode(response.body));
+    Map<String,dynamic> l = json.decode(response.body);
+    List<Snapshot> allSnapshot = new List();
+    // print(l);
+    l.forEach((k,v){
+      var snap = Snapshot.fromJson(v);
+      allSnapshot.add(snap);
+    });
+  
+    // print(allSnapshot);
+    return allSnapshot;
+  }
+
+
 }
 
 class Snapshot{
@@ -109,25 +126,3 @@ class Snapshot{
      'place\t:\t' + this.place + '\n' + ']';
   }
 }
-
-// Future<List<Beer>> getBeers() async {
-//   String url = 'https://api.punkapi.com/v2/beers?per_page=5';
-//   http.Response response = await http.get(url);
-
-//   // Try to convert json to dart object
-//   List<Beer> allBeers = new List();
-//   List<dynamic> beers = json.decode(response.body);
-//   for (var beerJson in beers) {
-//     var beer = Beer.fromJson(beerJson);
-//     allBeers.add(beer);
-//   }
-//   return allBeers;
-// }
-
-// {
-// 	“title”: “beach”,
-// 	“detail”: “going to the beach w/ family”,
-// 	“time”: “15.33”,
-// 	“place”: “”patong beach”,
-// 	"date": 20181225
-// }
